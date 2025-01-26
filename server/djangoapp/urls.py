0662396b3c240ca0a715f3697c961b6bde9a1c13
Sato-007 <satyaki.bose3@gmail.com>
2025-01-26 18:11:01 -0500

    Django Proxy

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
+
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
@@ -13,9 +14,9 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     # path for get_cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-
     # path for dealer reviews view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
